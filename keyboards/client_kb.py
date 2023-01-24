9d19d3fca08c5b11c1d5b515963139ffded9a315
SlowedWave <123001360+SlowedWave@users.noreply.github.com>
2023-01-24 17:38:10 +0300

    Delete keyboards directory

--- keyboards/client_kb.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
-
-b1 = KeyboardButton('/Режим_работы')
-b2 = KeyboardButton('/Адрес')
-b3 = KeyboardButton('Поделиться номером телефона', request_contact=True)
-
-kb_client = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-
-kb_client.row(b1, b2).add(b3)
